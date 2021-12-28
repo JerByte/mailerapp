ac4d8812e9f5dbaa9a600095a57d60300bd086a4
Lautaro <lautarorousseau@gmail.com>
2021-12-28 08:46:43 -0300

    ih

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import logging
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__, template_folder='/templates', static_folder='/static')
+    app = Flask(__name__, template_folder='templates')
 
     app.config.from_mapping(
         FROM_EMAIL=os.environ.get('FROM_EMAIL'),
